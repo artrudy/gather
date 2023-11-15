4f103b3b53cdb38bbc0c580469b9abf4144dd297
artёm rudoy <art.rudy@gmail.com>
2023-11-16 00:06:06 +0200

    form reset after submit and close form button

--- dist/gatheringList.view.js
+++ dist/gatheringList.view.js
@@ -3,95 +3,102 @@ import { onSubmitAttendance } from "./gatheringList.controller.js";
 import { createGathering } from "./gathering.model.js";
 const addGatherBtn = document.createElement("button");
 addGatherBtn.textContent = "+create new Gather";
+const newGatheringForm = document.createElement("form");
+let br = document.createElement("br");
+document.querySelector("#header")?.appendChild(newGatheringForm);
+const newGatheringFormTitleTag = document.createElement("label");
+newGatheringFormTitleTag.innerHTML = "Title:";
+const newGatheringFormTitle = document.createElement("input");
+newGatheringFormTitle.setAttribute("name", "title");
+const newGatheringFormLocationTag = document.createElement("label");
+newGatheringFormLocationTag.innerHTML = "Location:";
+const newGatheringFormLocation = document.createElement("input");
+newGatheringFormLocation.setAttribute("name", "location");
+const newGatheringFormStartTimeTag = document.createElement("label");
+newGatheringFormStartTimeTag.innerHTML = "Start time:";
+const newGatheringFormStartTime = document.createElement("input");
+newGatheringFormStartTime.setAttribute("type", "datetime-local");
+newGatheringFormStartTime.setAttribute("name", "start_time");
+const newGatheringFormdurationInHoursTag = document.createElement("label");
+newGatheringFormdurationInHoursTag.innerHTML = "Duration in hours:";
+const newGatheringFormdurationInHours = document.createElement("input");
+newGatheringFormdurationInHours.setAttribute("name", "duration_hours");
+newGatheringFormdurationInHours.setAttribute("type", "number");
+newGatheringFormdurationInHours.setAttribute("step", "0.1");
+const newGatheringFormOrganizerTag = document.createElement("label");
+newGatheringFormOrganizerTag.innerHTML = "Organizer:";
+const newGatheringFormOrganizer = document.createElement("input");
+newGatheringFormOrganizer.setAttribute("name", "organizer");
+const newGatheringFormParticipantLimitTag = document.createElement("label");
+newGatheringFormParticipantLimitTag.innerHTML = "Participant limit:";
+const newGatheringFormParticipantLimit = document.createElement("input");
+newGatheringFormParticipantLimit.setAttribute("name", "participant_limit");
+const newGatheringFormDescriptionTag = document.createElement("label");
+newGatheringFormDescriptionTag.innerHTML = "Description:";
+const newGatheringFormDescription = document.createElement("input");
+newGatheringFormDescription.setAttribute("name", "description");
+let btnTag = document.createElement("label");
+let submitBtn = document.createElement("button");
+btnTag.innerHTML = "Submit";
+submitBtn.appendChild(btnTag);
+newGatheringForm.appendChild(newGatheringFormTitleTag);
+newGatheringForm.appendChild(br.cloneNode());
+newGatheringForm.appendChild(newGatheringFormTitle);
+newGatheringForm.appendChild(br.cloneNode());
+newGatheringForm.appendChild(newGatheringFormLocationTag);
+newGatheringForm.appendChild(br.cloneNode());
+newGatheringForm.appendChild(newGatheringFormLocation);
+newGatheringForm.appendChild(br.cloneNode());
+newGatheringForm.appendChild(newGatheringFormStartTimeTag);
+newGatheringForm.appendChild(br.cloneNode());
+newGatheringForm.appendChild(newGatheringFormStartTime);
+newGatheringForm.appendChild(br.cloneNode());
+newGatheringForm.appendChild(newGatheringFormdurationInHoursTag);
+newGatheringForm.appendChild(br.cloneNode());
+newGatheringForm.appendChild(newGatheringFormdurationInHours);
+newGatheringForm.appendChild(br.cloneNode());
+newGatheringForm.appendChild(newGatheringFormOrganizerTag);
+newGatheringForm.appendChild(br.cloneNode());
+newGatheringForm.appendChild(newGatheringFormOrganizer);
+newGatheringForm.appendChild(br.cloneNode());
+newGatheringForm.appendChild(newGatheringFormParticipantLimitTag);
+newGatheringForm.appendChild(br.cloneNode());
+newGatheringForm.appendChild(newGatheringFormParticipantLimit);
+newGatheringForm.appendChild(br.cloneNode());
+newGatheringForm.appendChild(newGatheringFormDescriptionTag);
+newGatheringForm.appendChild(br.cloneNode());
+newGatheringForm.appendChild(newGatheringFormDescription);
+newGatheringForm.appendChild(br.cloneNode());
+newGatheringForm.appendChild(submitBtn);
+newGatheringForm.style.display = "none";
+submitBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    const formData = new FormData(newGatheringForm);
+    const gathering = {
+        id: "",
+        title: formData.get("title"),
+        location: formData.get("location"),
+        startTime: new Date(formData.get("start_time")),
+        durationInHours: parseFloat(formData.get("duration_hours")),
+        participantLimit: parseInt(formData.get("participant_limit"), 10),
+        organizer: formData.get("organizer"),
+        attendants: [],
+        description: formData.get("description"),
+    };
+    newGatheringForm.style.display = "none";
+    newGatheringForm.reset();
+    addGatherBtn.textContent = "+create new Gather";
+    createGathering(gathering);
+});
 addGatherBtn.addEventListener("click", (e) => {
-    let br = document.createElement("br");
-    const newGatheringForm = document.createElement("form");
-    document.querySelector("#header")?.appendChild(newGatheringForm);
-    const newGatheringFormTitleTag = document.createElement("label");
-    newGatheringFormTitleTag.innerHTML = "Title:";
-    const newGatheringFormTitle = document.createElement("input");
-    newGatheringFormTitle.setAttribute("name", "title");
-    const newGatheringFormLocationTag = document.createElement("label");
-    newGatheringFormLocationTag.innerHTML = "Location:";
-    const newGatheringFormLocation = document.createElement("input");
-    newGatheringFormLocation.setAttribute("name", "location");
-    const newGatheringFormStartTimeTag = document.createElement("label");
-    newGatheringFormStartTimeTag.innerHTML = "Start time:";
-    const newGatheringFormStartTime = document.createElement("input");
-    newGatheringFormStartTime.setAttribute("name", "start_time");
-    const newGatheringFormdurationInHoursTag = document.createElement("label");
-    newGatheringFormdurationInHoursTag.innerHTML = "Duration in hours:";
-    const newGatheringFormdurationInHours = document.createElement("input");
-    newGatheringFormdurationInHours.setAttribute("name", "duration_hours");
-    newGatheringFormdurationInHours.setAttribute("type", "number");
-    newGatheringFormdurationInHours.setAttribute("step", "0.1");
-    const newGatheringFormOrganizerTag = document.createElement("label");
-    newGatheringFormOrganizerTag.innerHTML = "Organizer:";
-    const newGatheringFormOrganizer = document.createElement("input");
-    newGatheringFormOrganizer.setAttribute("name", "organizer");
-    const newGatheringFormParticipantLimitTag = document.createElement("label");
-    newGatheringFormParticipantLimitTag.innerHTML = "Participant limit:";
-    const newGatheringFormParticipantLimit = document.createElement("input");
-    newGatheringFormParticipantLimit.setAttribute("name", "participant_limit");
-    const newGatheringFormDescriptionTag = document.createElement("label");
-    newGatheringFormDescriptionTag.innerHTML = "Description:";
-    const newGatheringFormDescription = document.createElement("input");
-    newGatheringFormDescription.setAttribute("name", "description");
-    let btnTag = document.createElement("label");
-    let submitBtn = document.createElement("button");
-    btnTag.innerHTML = "Submit";
-    // const formDisplayCheck = document.querySelector("form");
-    // console.log(formDisplayCheck);
-    // if (formDisplayCheck) {
-    //   newGatheringForm.style.display = "none";
-    // }
-    submitBtn.appendChild(btnTag);
-    newGatheringForm.appendChild(newGatheringFormTitleTag);
-    newGatheringForm.appendChild(br.cloneNode());
-    newGatheringForm.appendChild(newGatheringFormTitle);
-    newGatheringForm.appendChild(br.cloneNode());
-    newGatheringForm.appendChild(newGatheringFormLocationTag);
-    newGatheringForm.appendChild(br.cloneNode());
-    newGatheringForm.appendChild(newGatheringFormLocation);
-    newGatheringForm.appendChild(br.cloneNode());
-    newGatheringForm.appendChild(newGatheringFormStartTimeTag);
-    newGatheringForm.appendChild(br.cloneNode());
-    newGatheringForm.appendChild(newGatheringFormStartTime);
-    newGatheringForm.appendChild(br.cloneNode());
-    newGatheringForm.appendChild(newGatheringFormdurationInHoursTag);
-    newGatheringForm.appendChild(br.cloneNode());
-    newGatheringForm.appendChild(newGatheringFormdurationInHours);
-    newGatheringForm.appendChild(br.cloneNode());
-    newGatheringForm.appendChild(newGatheringFormOrganizerTag);
-    newGatheringForm.appendChild(br.cloneNode());
-    newGatheringForm.appendChild(newGatheringFormOrganizer);
-    newGatheringForm.appendChild(br.cloneNode());
-    newGatheringForm.appendChild(newGatheringFormParticipantLimitTag);
-    newGatheringForm.appendChild(br.cloneNode());
-    newGatheringForm.appendChild(newGatheringFormParticipantLimit);
-    newGatheringForm.appendChild(br.cloneNode());
-    newGatheringForm.appendChild(newGatheringFormDescriptionTag);
-    newGatheringForm.appendChild(br.cloneNode());
-    newGatheringForm.appendChild(newGatheringFormDescription);
-    newGatheringForm.appendChild(br.cloneNode());
-    newGatheringForm.appendChild(submitBtn);
-    submitBtn.addEventListener("click", (event) => {
-        event.preventDefault();
-        const formData = new FormData(newGatheringForm);
-        const gathering = {
-            id: "",
-            title: formData.get("title"),
-            location: formData.get("location"),
-            startTime: new Date(formData.get("start_time")),
-            durationInHours: parseFloat(formData.get("duration_hours")),
-            participantLimit: parseInt(formData.get("participant_limit"), 10),
-            organizer: formData.get("organizer"),
-            attendants: [],
-            description: formData.get("description"),
-        };
-        newGatheringForm.style.display = "none";
-        createGathering(gathering);
-    });
+    if (newGatheringForm.style.display === 'none' || newGatheringForm.style.display === '') {
+        newGatheringForm.style.display = 'block';
+        addGatherBtn.textContent = "Close form.";
+    }
+    else {
+        newGatheringForm.style.display = 'none';
+        addGatherBtn.textContent = "+create new Gather";
+    }
 });
 document.querySelector("#header")?.appendChild(addGatherBtn);
 //=====================
